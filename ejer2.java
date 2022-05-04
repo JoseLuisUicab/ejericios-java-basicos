53d44fc62e28191990e7c1f09a8e3f0345535dba
JoseLuisUicab <101893652+JoseLuisUicab@users.noreply.github.com>
2022-05-04 00:49:26 -0500

    Create ejer2.java

---
+++ ejer2.java
@@ -0,0 +1,4 @@
+//Se desea capturar los nombres de 10 personas que compitieron en un concurso
+//se capturan en el orden ascendente del primer lugar al décimo.
+//Pregunte al usuario si desea ver el nombre de la persona que ocupo el N lugar 
+//terminara el programa cuando el usuario dese salir.
